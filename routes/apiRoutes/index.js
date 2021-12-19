4a31be57b7ea7b46222ec764058a5feb32efbe63
Molly Montgomery <Molly@mollys-air-2.lan>
2021-12-18 18:57:35 -0600

    got note to save on localhost

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,18 +1,39 @@
 const router = require('express').Router()
 const path = require("path")
 const fs = require('fs');
+// https://www.npmjs.com/package/uuid
+const { v4: uuidv4 } = require('uuid');
 
-router.get("/notes", (req, res)=>(
+router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../../db/db.json"))
-))
+})
 
-router.post("/notes", (req, res)=>(
+router.post("/notes", (req, res) => {
+    const { title, text } = req.body;
 
+    const newNote = {
+        title: req.body.title,
+        test: req.body.text,
+        // id will generate random id calls from line 5
+        id: uuidv4()
+    };
 
-    fs.readFile(path.join(__dirname, "../../db/db.json"), 'utf-8', (err, data)=>{
+    console.log("newNote", newNote)
+
+    fs.readFile(path.join(__dirname, "../../db/db.json"), 'utf-8', (err, data) => {
         const parsedNotes = JSON.parse(data)
-        console.log("Data from db.json", parsedNotes)
+        parsedNotes.push(newNote)
+        console.log("data from db.json", parsedNotes)
+
+    fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(parsedNotes), (err) => {
+        if (err) throw err;
+        console.log("Saved!")
+        })
     })
-))
+
+    res.sendFile(path.join(__dirname, "../../db/db/json"))
+})
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const express = require('express');
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3002;
